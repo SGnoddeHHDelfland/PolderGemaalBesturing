d10e26a96aa9a35fe7e1c5ad9d7fb6741abb00c8
sgnodde <sgnodde@hhdelfland.nl>
2021-05-14 11:37:29 +0200

    json instead of hints.py

--- hints.py
+++
@@ -1,4 +0,0 @@
-def first_hint():
-    print("We are trying to make the reward high when the water level is high, and lower when it gets further away from that.")
-    print("So for example, ")
-    print("To make our lives a little easier, we turn this a")
